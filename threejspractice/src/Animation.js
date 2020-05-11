ebcc895c09e8b66e461f0411ea21d3cc88b57803
Craig Gant <craigkgant@gmail.com>
2020-05-11 08:34:06 -0400

    initial setup for threejs in animation

--- threejspractice/src/Animation.js
+++ threejspractice/src/Animation.js
@@ -1,24 +1,61 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useState, useRef, Component } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import React, { Component } from "react";
 import * as THREE from "three";
-import Controls from "./Controls";
 import "./styles.scss";
-import Skull from "./Skull";
+import SkullOne from "./SkullOne";
 
-export default function Animation() {
-  return (
-    <Canvas
-      camera={{ position: [10, 0, -25] }}
-      onCreated={({ gl }) => {
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
-      }}
-    >
-      <fog attach="fog" args={["#cc7b32", 2, 50]} />
-      <Controls />
-      <ambientLight intensity={0.4} />
-      <pointLight position={[-5, 5, 10]} penumbra={1} />
-      <Skull />
-    </Canvas>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.sceneSetup();
+    this.addSceneObjects();
+    this.startAnimationLoop();
+  }
+
+  // Scene setup
+  sceneSetup = () => {
+    const width = this.el.clientWidth;
+    const height = this.el.clientHeight;
+
+    this.scene = new THREE.Scene();
+    // perspective camera takes (fov, aspect, near, and far)
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 2, 500);
+    // positions camera away from objects
+    this.camera.position.z = 5;
+
+    // sets up renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(width, height);
+    // mount using React ref:
+    this.el.appendChild(this.renderer.domElement);
+  };
+  // Adding objects to scene
+  addSceneObjects = () => {
+    //start with a cube as placeholder
+    const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      flatShading: true,
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    // adds lighting to the scene
+    const ambientLight = new THREE.ambientLight(0x404040, 0.5);
+    this.scene.add(ambientLight);
+  };
+  // Starts animation loop
+  startAnimationLoop = () => {
+    this.cube.rotation.x += 0.01;
+
+    // brings in scene and camera to the renderer
+    this.renderer.render(this.scene, this.camera);
+    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
+  };
+
+  render() {
+    return <div ref={(this.el = ref)} />;
+  }
 }
--- threejspractice/src/SkullOne.js
+++ threejspractice/src/SkullOne.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "./styles.scss";
 
-const Skull = () => {
+const SkullOne = (props) => {
   const [model, setModel] = useState();
   // This Model was made by azzamah, and found at https://sketchfab.com/3d-models/card-130ec74a08b2445c91aae106d738d01e
   useEffect(() => {
@@ -11,8 +11,8 @@ const Skull = () => {
 
   // models returns if loaded
   return model ? (
-    <primitive object={model.scene} position={[0, -10, 0]} />
+    <primitive object={model.scene} position={props.position} />
   ) : null;
 };
 
-export default Skull;
+export default SkullOne;
